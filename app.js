1521b3988d571a325fdeef66921f195262f5153f
Raj Thombare <therajthombare@gmail.com>
2023-06-28 23:51:14 +0530

    add express Body Parser

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const http = require('http')
 
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs')
-    next(); // allows the request to continue to the next middleware in line
-})
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/add-product', (req, res, next) => {
-    console.log('In add product middleware!')
-    res.send('<h1>This is "Add Product" page</h1>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">add</button></form>')
+})
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
 })
 
 app.use('/', (req, res, next) => {
-    console.log('In another middleware!')
     res.send('<h1>Hello from Express!</h1>')
 })
 

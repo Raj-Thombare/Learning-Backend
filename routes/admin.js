dc2f039ef8759245d179e557c8f53411b61ade39
Raj Thombare <therajthombare@gmail.com>
2023-06-29 17:59:09 +0530

    serve css files statically

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,7 @@ router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'view', 'add-product.html'))
 })
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body)
     res.redirect('/')
 })

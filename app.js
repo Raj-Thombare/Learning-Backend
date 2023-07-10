20b8f39563eac74ddaf39ec917b106bb00c2637e
Raj Thombare <therajthombare@gmail.com>
2023-07-10 18:50:51 +0530

    added & implemented pug

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug') // set view engine to pug
+app.set('views', 'views') // set view location
+
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
@@ -15,7 +18,7 @@ app.use('/admin', adminData.router);
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'view', '404.html'))
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ const router = express.Router()
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'view', 'add-product.html'))
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {

2fac8c6e8e378e0718200add9490576d3f09193a
Raj Thombare <therajthombare@gmail.com>
2023-07-16 14:17:38 +0530

    add product & shop controller

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,14 @@
 const express = require('express')
-const path = require('path')
-const rootDir = require('../util/path')
 
 const router = express.Router()
+
+const productsController = require('../controllers/products')
 
 // if /add-products and /products both have a common path in front for eg.admin then you have to add
 // app.use('/admin' , adminRoutes) in app.js file
 
-const products = [];
+router.get('/add-product', productsController.getAddProduct)
 
-router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product', path: "/admin/add-product" })
-})
-
-router.post('/add-product', (req, res, next) => {
-    products.push({ title: req.body.title })
-    res.redirect('/')
-})
+router.post('/add-product', productsController.postAddProduct)
 
 exports.router = router;
-exports.products = products;

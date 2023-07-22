f61ffbab0991b626bb785db3d1c0b75667e824da
Raj Thombare <therajthombare@gmail.com>
2023-07-22 21:40:06 +0530

    store & read using file system

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require("fs")
+const path = require("path")
 
 module.exports = class Product {
 
@@ -7,10 +8,34 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this)
+        // C: \Users\rajth\Dev\Learning - Backend
+
+        let products = [];
+        const p = path.join(
+            path.dirname(require.main.filename), "data", "products.json"
+        )
+        fs.readFile(p, (err, fileContent) => {
+            if (!err) {
+                products = JSON.parse(fileContent)
+            }
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            })
+        })
+
     }
 
-    static fetchAll() {
-        return products
+    static fetchAll(cb) {
+        const p = path.join(
+            path.dirname(require.main.filename), "data", "products.json"
+        )
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb([])
+            } else {
+                cb(JSON.parse(fileContent))
+            }
+        })
     }
 }

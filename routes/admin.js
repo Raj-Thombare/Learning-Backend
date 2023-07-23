1384d1291ce1d708a534fae8d7b148adf0e2f11e
Raj Thombare <therajthombare@gmail.com>
2023-07-23 20:45:58 +0530

    project setup

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,18 @@ const express = require('express')
 
 const router = express.Router()
 
-const productsController = require('../controllers/products')
+const adminController = require('../controllers/admin');
 
 // if /add-products and /products both have a common path in front for eg.admin then you have to add
 // app.use('/admin' , adminRoutes) in app.js file
 
-router.get('/add-product', productsController.getAddProduct)
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', productsController.postAddProduct)
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;

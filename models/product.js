7c90893634a60bd59551c3610c81736b99c0f47b
Raj Thombare <therajthombare@gmail.com>
2023-07-31 15:29:53 +0530

    add Product Delete functionality

--- models/product.js
+++ models/product.js
@@ -45,13 +45,24 @@ module.exports = class Product {
         })
     }
 
+    static deletById(id) {
+        getProductsFromFile(products => {
+            const updatedProducts = products.filter(prod => prod.id !== id);
+            fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+                if (!err) {
+
+                }
+            })
+        })
+    }
+
     static fetchAll(cb) {
         getProductsFromFile(cb)
     }
 
     static findById(id, cb) {
         getProductsFromFile(products => {
-            const product = products.find(p => p.id == id);
+            const product = products.find(p => p.id === id);
             cb(product);
         })
     }

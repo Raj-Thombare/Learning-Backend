4cf80d08090774ad09b4584023b72fd43c02d589
Raj Thombare <therajthombare@gmail.com>
2023-08-04 16:43:35 +0530

    connect to db & insert data

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error')
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
@@ -22,8 +23,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public'))); //sets up a middleware in Express to serve static files
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes)
+// app.use(shopRoutes)
 
 app.use(errorController.get404)
 
-app.listen(3000)
+mongoConnect(() => {
+    app.listen(3000)
+})
+
+// app.listen(3000);
+
+
--- models/product.js
+++ models/product.js
@@ -1,72 +1,91 @@
-const fs = require("fs")
-const path = require("path")
+// const fs = require("fs")
+// const path = require("path")
 
-const Cart = require('./cart');
+// const Cart = require('./cart')
+const getDb = require('../util/database').getDb;
 
-const p = path.join(
-    path.dirname(require.main.filename), "data", "products.json"
-)
-
-const getProductsFromFile = (cb) => {
-    fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            cb([])
-        } else {
-            cb(JSON.parse(fileContent))
-        }
-    })
-}
+// const p = path.join(
+//     path.dirname(require.main.filename), "data", "products.json"
+// )
 
 module.exports = class Product {
-
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
+    constructor(title, price, description, imgUrl) {
         this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
         this.price = price;
+        this.description = description;
+        this.imgUrl = imgUrl;
     }
-    // C: \Users\rajth\Dev\Learning - Backend
 
     save() {
-        getProductsFromFile((products) => {
-            if (this.id) {
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-                const updatedProducts = [...products];
-                updatedProducts[existingProductIndex] = this;
-                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-                    console.log(err)
-                })
-            } else {
-                this.id = Math.random().toString();
-                products.push(this)
-                fs.writeFile(p, JSON.stringify(products), (err) => {
-                    console.log(err)
-                })
-            }
-        })
-    }
-
-    static deleteById(id) {
-        getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id);
-            const updatedProducts = products.filter(prod => prod.id !== id);
-            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-                if (!err) {
-                    Cart.deleteProduct(id, product.price);
-                }
-            });
+        const db = getDb();
+        return db.collection('products').insertOne(this).then(result => {
+            console.log('Product inserted:', result)
+        }).catch(err => {
+            console.log('Error inserting product:', err);
         });
     }
-
-    static fetchAll(cb) {
-        getProductsFromFile(cb)
-    }
-
-    static findById(id, cb) {
-        getProductsFromFile(products => {
-            const product = products.find(p => p.id === id);
-            cb(product);
-        })
-    }
 }
+
+// const getProductsFromFile = (cb) => {
+//     fs.readFile(p, (err, fileContent) => {
+//         if (err) {
+//             cb([])
+//         } else {
+//             cb(JSON.parse(fileContent))
+//         }
+//     })
+// }
+
+// module.exports = class Product {
+
+//     constructor(id, title, imageUrl, description, price) {
+//         this.id = id;
+//         this.title = title;
+//         this.imageUrl = imageUrl;
+//         this.description = description;
+//         this.price = price;
+//     }
+//     // C: \Users\rajth\Dev\Learning - Backend
+
+//     save() {
+//         getProductsFromFile((products) => {
+//             if (this.id) {
+//                 const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+//                 const updatedProducts = [...products];
+//                 updatedProducts[existingProductIndex] = this;
+//                 fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+//                     console.log(err)
+//                 })
+//             } else {
+//                 this.id = Math.random().toString();
+//                 products.push(this)
+//                 fs.writeFile(p, JSON.stringify(products), (err) => {
+//                     console.log(err)
+//                 })
+//             }
+//         })
+//     }
+
+//     static deleteById(id) {
+//         getProductsFromFile(products => {
+//             const product = products.find(prod => prod.id === id);
+//             const updatedProducts = products.filter(prod => prod.id !== id);
+//             fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+//                 if (!err) {
+//                     Cart.deleteProduct(id, product.price);
+//                 }
+//             });
+//         });
+//     }
+
+//     static fetchAll(cb) {
+//         getProductsFromFile(cb)
+//     }
+
+//     static findById(id, cb) {
+//         getProductsFromFile(products => {
+//             const product = products.find(p => p.id === id);
+//             cb(product);
+//         })
+//     }
+// }

f6ddead4e0403bd32395b97551460a695878c137
Raj Thombare <therajthombare@gmail.com>
2023-08-10 19:32:45 +0530

    display all cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -42,34 +42,23 @@ exports.getIndex = (req, res, next) => {
         });
 };
 
-// exports.getCart = (req, res, next) => {
-//     Cart.getCart(cart => {
-//         Product.fetchAll(products => {
-//             const cartProducts = [];
-//             for (product of products) {
-//                 const cartProductData = cart.products.find(
-//                     prod => prod.id === product.id
-//                 );
-//                 if (cartProductData) {
-//                     cartProducts.push({ productData: product, qty: cartProductData.qty });
-//                 }
-//             }
-//             res.render('shop/cart', {
-//                 path: '/cart',
-//                 pageTitle: 'Your Cart',
-//                 products: cartProducts
-//             });
-//         });
-//     });
-// };
+exports.getCart = (req, res, next) => {
+    req.user.getCart().then(products => {
+        res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
+        })
+    }).catch(err => console.log(err))
+};
 
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
-    console.log("prodID", prodId)
     Product.findById(prodId).then(product => {
         return req.user.addToCart(product);
     }).then(result => {
         console.log('result is ', result)
+        res.redirect('/cart')
     })
     // Product.findById(prodId, product => {
     //     Cart.addProduct(prodId, product.price);

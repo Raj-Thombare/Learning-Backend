d5283e2d010c8fb5d9b77d2ec0c62eacfbb95d03
Raj Thombare <therajthombare@gmail.com>
2023-08-13 21:13:52 +0530

    get orders in order page

--- controllers/shop.js
+++ controllers/shop.js
@@ -84,15 +84,13 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders'
-    });
+    req.user.getOrders().then(orders => {
+        res.render('shop/orders', {
+            path: '/orders',
+            pageTitle: 'Your Orders',
+            orders: orders
+        });
+    }).catch(err => console.log(err))
 };
 
-// exports.getCheckout = (req, res, next) => {
-//     res.render('shop/checkout', {
-//         path: '/checkout',
-//         pageTitle: 'Checkout'
-//     });
-// };
+

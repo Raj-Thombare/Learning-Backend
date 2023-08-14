e7cdb13e1f39319a6c9fd7a2803d93cb09b618a2
Raj Thombare <therajthombare@gmail.com>
2023-08-14 18:41:50 +0530

    connect to db using mongoose

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path')
+const mongoose = require('mongoose')
 
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error')
-const mongoConnect = require('./util/database').mongoConnect;
+// const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user')
 
 const app = express();
@@ -23,22 +24,25 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, 'public'))); //sets up a middleware in Express to serve static files
 
-app.use((req, res, next) => {
-    User.findById('64d8deaf4de4994306441e4f').then((user) => {
-        req.user = new User(user.name, user.email, user.cart, user._id);
-        next();
-    }).catch(err => console.log(err))
-})
+// app.use((req, res, next) => {
+//     User.findById('64d8deaf4de4994306441e4f').then((user) => {
+//         req.user = new User(user.name, user.email, user.cart, user._id);
+//         next();
+//     }).catch(err => console.log(err))
+// })
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes)
 
 app.use(errorController.get404)
 
-mongoConnect(() => {
+mongoose.connect('mongodb+srv://Raj:ogsFAMR9sJ5Rh1rW@cluster0.f7mohae.mongodb.net/shop?retryWrites=true&w=majority').then(result => {
     app.listen(3000)
-})
+}).catch(err => console.log(err))
 
-// app.listen(3000);
+// mongoConnect(() => {
+//     app.listen(3000)
+// })
+
 
 

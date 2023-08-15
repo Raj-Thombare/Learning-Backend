9b48180cb76ec8a5c2df051a374ffbcee2c6920d
Raj Thombare <therajthombare@gmail.com>
2023-08-15 20:33:35 +0530

    add product to cart

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ const userSchema = new Schema({
         items: [{
             productId: {
                 type: Schema.Types.ObjectId,
-                ref:  'Product',
+                ref: 'Product',
                 required: true
             },
             quantity: {
@@ -23,8 +23,31 @@ const userSchema = new Schema({
                 required: true
             }
         }]
-    }
+    },
 })
+
+userSchema.methods.addToCart = function (product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+        return cp.productId.toString() === product._id.toString();
+    })
+
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items]
+
+    if (cartProductIndex >= 0) {
+        newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+        updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+        updatedCartItems.push({
+            productId: product._id,
+            quantity: newQuantity
+        })
+    }
+
+    const updatedCart = { items: updatedCartItems }
+    this.cart = updatedCart;
+    return this.save();
+}
 
 module.exports = mongoose.model('User', userSchema)
 

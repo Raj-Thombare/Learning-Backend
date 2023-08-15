73496e3e581c81892d9c4cd62d3f21b88b988f1d
Raj Thombare <therajthombare@gmail.com>
2023-08-15 20:58:55 +0530

    get products in cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -42,7 +42,9 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    req.user.getCart().then(products => {
+    req.user.populate('cart.items.productId').then(user => {
+        const products = user.cart.items;
+        console.log(products)
         res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',

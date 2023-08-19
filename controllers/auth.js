00ec34f15feaa12ef68ae73545863aa6ce82e4bf
Raj Thombare <therajthombare@gmail.com>
2023-08-19 20:00:47 +0530

    setup cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,13 @@
 exports.getLogin = (req, res, next) => {
+  console.log(req.get("Cookie"));
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
+    isAuthenticated: req.isLoggedIn,
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.cookie("LoggedIn", "true", { maxAge: 3600000, httpOnly: true });
+  res.redirect("/");
 };

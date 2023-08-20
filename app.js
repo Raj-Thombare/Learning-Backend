bb2099bc61f87c4be5fbf6542139e507ed66289d
Raj Thombare <therajthombare@gmail.com>
2023-08-20 20:34:02 +0530

    add user in db with hash pass

--- app.js
+++ app.js
@@ -64,18 +64,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Rakaa",
-          email: "rakaa@test.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
     console.log("DB Connected!");
     app.listen(3000);
   })

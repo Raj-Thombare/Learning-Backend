b9e025632e1730e042e528ecd697ee7078fd67a9
Raj Thombare <therajthombare@gmail.com>
2023-08-23 03:38:22 +0530

    add authorization

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport(
   sendGridTransport({
     auth: {
       api_key:
-        "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",
+        "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",
     },
   })
 );
@@ -155,4 +155,61 @@ exports.postReset = (req, res, next) => {
         console.log(err);
       });
   });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({
+    resetToken: token,
+    resetTokenExpiration: { $gt: Date.now() },
+  })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        isAuthenticated: false,
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetuser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      return res.redirect("/login");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
